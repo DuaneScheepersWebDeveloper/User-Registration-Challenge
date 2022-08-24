fdc892fe88dc857c7fd1fcb13ca1be31a9a643b8
duaneo2011 <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2022-08-24 10:00:37 +0200

    Added the functionality that allows us to display user
    Added the functionality that allows us to display user

--- src/js/user-register.js
+++ src/js/user-register.js
@@ -1,42 +1,60 @@
-/* ----------------------------------------------------
-    Setup
----------------------------------------------------- */
+const registerTab = document.querySelector('.register');
+const userTabs = document.querySelector('.users');
+const registerButton = document.querySelector('#users-tab-button');
+const userListButton = document.querySelector('#register-tab-button');
+//---------------------------------------------------------------------
+const firstName = document.querySelector('#firstname').value;
+console.log(firstName);
+const lastName = document.querySelector('#lastname');
+const userName = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+//---------------------------------------------------------------------
+const submit = document.getElementById('register-submit');
+//---------------------------------------------------------------------
+registerButton.addEventListener('click', (e) => {
+	userTabs.style.display = 'none';
+	registerTab.style.display = 'block';
+});
+userListButton.addEventListener('click', (e) => {
+	userTabs.style.display = 'block';
+	registerTab.style.display = 'none';
+});
 
-/* --- Tab References --- */
+//---------------------------------------------------------------------
+const userList = {
+	firstName: 'John',
+	lastName: 'Doe',
+	userName: 'johndoe7',
+	email: 'example.com',
+	password: ' ba7816bf8f01',
+};
 
-    // Get reference to all elements in register tab
-    const registerTab = document.getElementsByClassName("register");
-    console.log(registerTab);
+const newUserList = [];
+newUserStart = () => {
+	newUserList.push(userList);
+};
+newUserStart();
+const newUser = () => {
+	console.log();
+};
+//---------------------------------------------------------------------
+const addUser = () => {};
+addUser();
 
-    // Get reference to all elements in userList tab
-    const userTabs = document.getElementsByClassName("users");
-    console.log(userTabs);
-
-
-/* --- Nav references --- */
-
-    // code here...
-
-
-/* --- Form field references --- */
-
-    // code here...
-
-
-/* --- Table list reference --- */
-
-    // code here...
-
-
-
-/* ----------------------------------------------------
-    Functionality
----------------------------------------------------- */
-
-    // code here...
-
-
-
-/* ----------------------------------------------------
-    Event Listenters and Interactivity
----------------------------------------------------- */
+//---------------------------------------------------------------------
+const displayUsers = () => {
+	for (let i = 0; i < newUserList.length; i++) {
+		console.log(newUserList[i]);
+		document.getElementById('user-list').innerHTML += `
+    <li class="entry">
+		<span>${newUserList[i].userName}</span>
+		<span>${newUserList[i].firstName}</span>
+		<span>${newUserList[i].lastName}</span>
+		<span>${newUserList[i].email}</span>
+		<span>${newUserList[i].password}</span>
+	</li>
+    `;
+	}
+};
+displayUsers();
---
+++ src/js/userList.js
@@ -0,0 +1,9 @@
+const userList = {
+	firstName: 'John',
+	lastName: 'Doe',
+	userName: 'johndoe7',
+	email: 'example.com',
+	password: ' ba7816bf8f01',
+};
+
+export default userList;
